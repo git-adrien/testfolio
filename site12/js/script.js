bb2ddc4536ca0db8f74811c3007225dccab0f7f5
Mrtruite <fevreadrien.info@gmail>
2021-04-05 02:22:11 +0200

    update navigation/page transition

--- site12/js/script.js
+++ site12/js/script.js
@@ -1,24 +1,18 @@
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
-  
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-      return;
-    }
-  
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 1.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
-  }
-  
-  window.onload = main;
 
 
 
+  window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 400)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    setTimeout(function() { 
+        document.querySelector('body').style.opacity = 1
+    }, 150)
+})
 
 
 const scroll = new LocomotiveScroll({
