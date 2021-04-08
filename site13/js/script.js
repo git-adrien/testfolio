53550188c7185d24eec9b16604138c5531f79cb5
Mrtruite <fevreadrien.info@gmail>
2021-04-08 23:40:01 +0200

    Update to V13

---
+++ site13/js/script.js
@@ -0,0 +1,82 @@
+
+
+
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        //window.open()
+        window.location.href = href
+    }, 400)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    setTimeout(function() { 
+        document.querySelector('body').style.opacity = 1
+    }, 150)
+})
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    if(event.target.classList.contains('hovermouse')){
+        //cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - (event.toElement.clientWidth/2))+"px;width:"+event.toElement.clientWidth + "px")
+        cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - 15)+"px;")
+        cursor.classList.add("expand");
+    }else{
+        cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - 15)+"px;")
+        cursor.classList.remove("expand");
+    }
+    
+})
+
+document.addEventListener("mouseover", function( event ){
+   imgCursor = event.target.classList.contains('hovermouseproject')
+   linkBg = event.target
+    bg = this.getElementById('projectBG') ;
+    if(imgCursor){
+        img = event.target.dataset.img
+        bg.style.backgroundImage = "url('"+img+"')";
+        linkBg.classList.add("bgPro");
+    }
+    
+})
+
+
+
+
+
+
+function update() {
+    clock = document.getElementById("clock") 
+    clock.innerHTML = moment().format('H:mm')
+}
+  setInterval(update, 1000);
+
+  LocalTime = document.getElementById("fuClock") 
+  LocalTime.innerHTML = moment.locale();
+  yearTime = document.getElementById("yearClock") 
+  yearTime.innerHTML = moment().format('YYYY');   ;
+  
+  update()
+/**
+ * document.addEventListener("mouseover", function( event ){
+   imgCursor = event.target.classList.contains('hovermouseproject')
+    bg = this.getElementById('projectBG') ;
+    if(imgCursor){
+        img = event.target.dataset.img
+        cursor.classList.add("imgCursor");
+        cursor.style.backgroundImage = "url('"+img+"')";
+    }else{
+        img = event.target.dataset.img
+        cursor.classList.remove("imgCursor");
+        cursor.style.backgroundImage = "url('"+img+"')";
+    }
+})
+ */
