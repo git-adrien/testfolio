ab5e29d5f838a07dd4e86b091cc3fc1345878be5
Adrien <fevreadrien.info@gmail.com>
2021-09-16 13:49:16 +0200

    update

---
+++ site14/P5js copie/Test1/sketch.js
@@ -0,0 +1,49 @@
+
+
+var video;
+var vScale = 16;
+
+function setup() {
+    let cnv = createCanvas(2048, 1792);
+    pixelDensity(1);
+    
+    video = createCapture(VIDEO);
+    video.size(width / vScale, height / vScale);
+  }
+  
+  function draw() {
+    background(255)
+    video.loadPixels();
+    for (var y = 0; y < video.height; y++) {
+      for (var x = 0; x < video.width; x++) {
+        var index = (video.width - x + 1 + (y * video.width)) * 4;
+        var r = video.pixels[index + 0];
+        var g = video.pixels[index + 1];
+        var b = video.pixels[index + 2];
+
+        var bright = (r + g + b)/3;
+
+        var w = map(bright, 127, 255, 127, vScale);
+        
+        noStroke();
+        if(bright<63){
+          fill(224, 248, 208);
+        }else if(bright<126){
+          fill(136, 192, 112);
+        }else if(bright<189){
+          fill(52, 104, 86);
+        }else if(bright<255){
+          fill(8, 24, 32);
+        }
+        
+        
+        
+       
+
+        rectMode(CENTER);
+
+   
+        rect(x * vScale, y * vScale, vScale-5, vScale-5);
+      }
+    }
+  }
---
+++ site14/P5js copie/Test2/sketch.js
@@ -0,0 +1,20 @@
+let mic;
+
+ function setup(){
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw(){
+  background(0);
+  fill(255);
+  text('tap to start', width/2, 20);
+
+  micLevel = mic.getLevel();
+  console.log(micLevel*1000)
+  let y = height - micLevel * height;
+  ellipse(width/2, y, 10, 10);
+}
