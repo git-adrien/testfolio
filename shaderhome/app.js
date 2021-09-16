b5ed0d1afc0d042c87a485de7cf490292cac105f
Adrien <fevreadrien.info@gmail.com>
2021-09-17 01:37:56 +0200

    update app.js node module path

--- shaderhome/app.js
+++ shaderhome/app.js
@@ -1,5 +1,4 @@
-import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from './node_modules/three'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import testTexture from './plop2.jpg'
--- shaderhome/app2.js
+++ shaderhome/app2.js
@@ -1,9 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from './node_modules/three'
 
-
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
-
+import { TrackballControls } from './node_modules/three/examples/jsm/controls/TrackballControls'
 
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
